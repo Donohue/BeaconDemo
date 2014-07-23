4f2c41bbd931a705081a1e0711f28e2c8dee7373
Brian Donohue <brian.donohue89@gmail.com>
2014-07-23 18:47:09 -0400

    Initial Commit

---
+++ BeaconDemo/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  BeaconDemo
+//
+//  Created by Brian Donohue on 7/23/14.
+//  Copyright (c) 2014 Brian Donohue. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
